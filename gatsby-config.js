614642e2ce118473954c995111d363bca32c05b0
Hendrik Mölder <hendrik@molder.eu>
2018-09-08 17:28:57 +0100

    Update config for Netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
             resolve: `gatsby-source-filesystem`,
             options: {
                 name: `content`,
-                path: `src/content/`,
+                path: `${__dirname}/src/content/`,
             },
         },
         'gatsby-plugin-netlify-cms',
