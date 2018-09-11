23893c541479bbae011aacde53b2ad1938eb5117
Hendrik Molder <hendrik@molder.eu>
2018-09-11 08:11:21 +0100

    Add nav links

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome, faHeadphonesAlt } from '@fortawesome/free-solid-svg-icons'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
 import  Link  from 'gatsby-link'
 
 import navigationStyles from './navigation.module.css'
@@ -10,7 +10,9 @@ export default class Navigation extends Component {
         return (
             <div className={navigationStyles.container}>
                 <Link to="/" className={navigationStyles.link}><FontAwesomeIcon icon={faHome} /></Link>
-                <Link to="/music" className={navigationStyles.link}><FontAwesomeIcon icon={faHeadphonesAlt} /> Music</Link>
+                <Link to="/music" className={navigationStyles.link}>Music</Link>
+                <a href="https://linkedin.com/in/hendrikmolder" className={navigationStyles.link} alt="LinkedIn">LinkedIn</a>
+                <a href="https://github.com/hendrikmolder" className={navigationStyles.link} alt="LinkedIn">GitHub</a>
             </div>
         )
     }
