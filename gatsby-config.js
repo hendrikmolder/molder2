3cf5d9f4cf3c90b9fc73f2c725d84c2200a40966
Hendrik Mölder <hendrik@molder.eu>
2019-01-19 15:53:19 +0000

    Remove typography.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,6 @@
 module.exports = {
     plugins: [
         {
-            resolve: 'gatsby-plugin-typography',
-            options: {
-                pathToConfigModule: `src/utils/typography.js`,
-            },
-        },
-        {
             resolve: `gatsby-source-filesystem`,
             options: {
                 name: `content`,
@@ -18,7 +12,15 @@ module.exports = {
         {
             resolve: `gatsby-plugin-favicon`,
             options: {
-              logo: "./static/favicon.png",
+                logo: "./static/favicon.png",
+                plugins: [
+                    {
+                        resolve: `gatsby-remark-prismjs`,
+                        options: {
+                            classPrefix: 'language-',
+                        }
+                    }
+                ]
             }
         },
         `gatsby-transformer-json`,
