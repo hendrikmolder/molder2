22ff9b42160c5453f730c80acc7024093f650e75
Hendrik Mölder <hendrik@molder.eu>
2019-02-02 13:08:53 +0000

    Add StaticNav

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -5,6 +5,7 @@ import { graphql, StaticQuery } from 'gatsby'
 import PropTypes from 'prop-types'
 
 import navigationStyles from './navigation.module.scss'
+import staticLinks from './links.json'
 
 const Navigation = ({data}) => (
     <div className={navigationStyles.container}>
@@ -18,6 +19,20 @@ const Navigation = ({data}) => (
         })}
     </div>
 )
+
+export const StaticNavigation = () => (
+    <div className={navigationStyles.container}>
+        {staticLinks.map((node, key) => {
+            const { name, icon, href } = node
+
+            /* Use <a> for external href */
+            return (href[0] !== "/")
+                ? <a key={key} href={href}>{icon && <Icon name={icon} />} {name && name}</a>
+                : <Link key={key} to={href}>{icon && <Icon name={icon} />} {name && name}</Link>
+        })}
+    </div>
+)
+
 
 export default insertProps => (
     <StaticQuery query={query} render={data => <Navigation data={data} {...insertProps} />} />
