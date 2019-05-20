b2fb56434b49f590195e99ab74f84e2da37884bf
Hendrik Mölder <hendrik@molder.eu>
2019-05-20 22:38:11 +0100

    Remove navigation

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -7,19 +7,6 @@ import PropTypes from 'prop-types'
 import navigationStyles from './navigation.module.scss'
 import staticLinks from './links.json'
 
-const Navigation = ({data}) => (
-    <div className={navigationStyles.container}>
-        { data.allLinksJson && data.allLinksJson.edges.map((node, key) => {
-            const { name, icon, href } = node.node
-
-            /* Use <a> for external href */
-            return (href[0] !== "/")
-                ? <a key={key} href={href}>{icon && <Icon name={icon} />} {name && name}</a>
-                : <Link key={key} to={href}>{icon && <Icon name={icon} />} {name && name}</Link>
-        })}
-    </div>
-)
-
 export const StaticNavigation = () => (
     <div className={navigationStyles.container}>
         {staticLinks.map((node, key) => {
@@ -37,21 +24,6 @@ export const StaticNavigation = () => (
 export default insertProps => (
     <StaticQuery query={query} render={data => <Navigation data={data} {...insertProps} />} />
 )
-
-const query = graphql`
-    query {
-        allLinksJson(sort: { fields: ordering}) {
-            edges {
-                node {
-                    name
-                    icon
-                    href
-                    ordering
-                }
-            }
-        }
-    }
-`
 
 Navigation.propTypes = {
     data: PropTypes.shape({
