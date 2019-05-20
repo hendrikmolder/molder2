5da9f1e7b048f4c85b2f79356cb01ffee45c68e5
Hendrik Mölder <hendrik@molder.eu>
2019-05-20 22:38:01 +0100

    Add SubTitle to Layout

--- src/components/Layout/index.spec.js
+++ src/components/Layout/index.spec.js
@@ -4,11 +4,12 @@ import renderer from 'react-test-renderer'
 import Layout from './index'
 
 const TEST_TITLE = 'Test'
+const TEST_SUBTITLE = 'test'
 
 describe('Layout', () => {
     it('renders correctly', () => {
         const tree = renderer
-            .create(<Layout title={TEST_TITLE}><p>test</p></Layout>)
+            .create(<Layout title={TEST_TITLE} subTitle={TEST_SUBTITLE}><p>test</p></Layout>)
             .toJSON()
         expect(tree).toMatchSnapshot()
     })
