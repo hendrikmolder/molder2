2370df84fb8983cc399c8a2625008ee0df708f68
Hendrik Mölder <hendrik@molder.eu>
2019-05-23 14:58:55 +0100

    Update landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,23 @@
 import React from 'react'
 import styled from 'styled-components'
-import Helmet from 'react-helmet'
 
+import Layout from '../components/Layout'
 import Logo from '../components/Logo'
-import './styles/global.module.scss'
 
-const Layout = styled.div`
-    text-align: center;
-    /* position: absolute; temporary hack */
-    height: 100%;
-    width: 100% !important;
-`
+import './styles/global.module.scss'
 
 const StyledLogo = styled(Logo)`
     margin-top: 216px ;
 `
 
+const StyledLayout = styled(Layout)`
+    text-align: center;
+`
 
 const Landing = () => (
-    <Layout>
-        <Helmet>
-            <title>molder</title>
-        </Helmet>
+    <StyledLayout text>
         <StyledLogo width={486}/>
-    </Layout>
+    </StyledLayout>
 )
 
 export default Landing
