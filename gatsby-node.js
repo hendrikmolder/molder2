972216cd90fbe4f09e4683ee5f6aebae9af7bee1
Hendrik Mölder <hendrik@molder.eu>
2019-05-23 22:06:23 +0100

    Implement Pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,23 +1,10 @@
-// const { createFilePath } = require(`gatsby-source-filesystem`)
 const path = require('path')
 const linkResolver = require('./src/utils/linkResolver')
-
-// exports.onCreateNode = ({ node, getNode, actions }) => {
-//     const { createNodeField } = actions
-//     if (node.internal.type === 'MarkdownRemark') {
-//         const slug = createFilePath({ node, getNode, basePath: 'pages' })
-
-//         createNodeField({
-//             node,
-//             name: 'slug',
-//             value: slug,
-//         })
-//     }
-// }
 
 exports.createPages = async ({ graphql, actions }) => {
     const { createPage } = actions
     const postTemplate = path.resolve('./src/templates/post.js')
+    const pageTemplate = path.resolve('./src/templates/page.js')
 
     const pages = await graphql(`{
         allPrismicPosts {
@@ -28,15 +15,29 @@ exports.createPages = async ({ graphql, actions }) => {
                 }
             }
         }
+        allPrismicPage {
+            edges {
+                node {
+                    type
+                    uid
+                }
+            }
+        }
     }`)
 
-    pages.data.allPrismicPosts.edges.forEach(edge => {
-        createPage({
-            path: linkResolver(edge.node),
-            component: postTemplate,
-            context: {
-                uid: edge.node.uid,
-            },
-        })
-    })
+    pages.data.allPrismicPosts.edges.forEach(edge => createPage({
+        path: linkResolver(edge.node),
+        component: postTemplate,
+        context: {
+            uid: edge.node.uid,
+        },
+    }))
+
+    pages.data.allPrismicPage.edges.forEach(edge => createPage({
+        path: linkResolver(edge.node),
+        component: pageTemplate,
+        context: {
+            uid: edge.node.uid,
+        },
+    }))
 }
---
+++ src/templates/page.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout'
+
+export default ({ data: { prismicPage } }) => {
+    const { data } = prismicPage
+
+    return (
+        <React.Fragment>
+            <Layout title={data.title.text} subTitle={data.subtitle.text} text>
+                <div dangerouslySetInnerHTML={{ __html: data.body.html }} />
+            </Layout>
+        </React.Fragment>
+    )
+}
+
+export const pageQuery = graphql`
+    query PageByUid($uid: String!) {
+        prismicPage(uid: { eq: $uid }) {
+            uid
+            data {
+                title { text }
+                subtitle { text }
+                body { html }
+                auhor { uid }
+                last_updated
+            }
+        }
+    }
+`
