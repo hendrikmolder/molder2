b018fe60ae2eacddf05a1d01624c9233c8d0335d
Hendrik Mölder <hendrik@molder.eu>
2019-05-23 14:58:31 +0100

    Use Semantic Container

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -2,50 +2,25 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import Helmet from 'react-helmet'
-import Link from 'gatsby-link'
+import { Container } from 'semantic-ui-react'
+
+import SiteNavigation from '../SiteNavigation'
 
 import 'prismjs/themes/prism-solarizedlight.css'
 import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
 import 'semantic-ui-css/semantic.min.css' /* Import Semantic-UI CSS */
 
-const Container = styled.div`
-    /* padding: 40px; */
-    width: 950px;
+const MENU_ITEMS = [
+    { name: "bio", href: "/bio" },
+    { name: "blog", href: "/blog" },
+]
+
+const StyledContainer = styled(Container)`
     min-height: 100%;
-    margin: 60px auto 0;
-
-    display: flex;
-    justify-content: space-between;
-`
-
-const MenuContainer = styled.div`
-    margin: 155px 0 0;
-    width: 110px;
-    text-decoration: none;
-    text-align: right;
-    font-weight: bold;
-    font-size: 18px;
-    display: flex;
-    flex-direction: column;
-
-    >a {
-        text-decoration: none;
-        color: #000000;
-        margin: 0 0 6px;
-    }
-`
-
-const MetaContainer = styled.div`
-    min-height: 48px;
-    margin: 0 0 38px;
-`
-
-const ContentContainer = styled.div`
-    width: 770px;
-    height: 100%;
+    margin: 60px auto 100px;
 
     img, iframe, pre, code {
-        max-width: 100%;
+        width: 100%;
     }
 
     a {
@@ -54,34 +29,35 @@ const ContentContainer = styled.div`
     }
 `
 
+const MetaContainer = styled.div`
+    min-height: 48px;
+    margin: 0 0 38px;
+`
+
 const SubTitle = styled.h2`
     color: rgb(132, 132, 132);
-    font-size: 18px;
+    font-size: 20px;
     font-weight: 300;
+    margin: 0 0 36px;
 `
 
 const Page = (props) => {
-    const { children, title, subTitle, meta } = props
+    const { children, title, subTitle, meta, text } = props
 
     return (
-        <Container>
-            <Helmet>
-                <title>molder | { title }</title>
-            </Helmet>
+        <React.Fragment>
+            <SiteNavigation items={MENU_ITEMS} />
+            <StyledContainer text={text && text}>
+                <Helmet>
+                    <title>{ title ? `${title} | molder` : `molder` }</title>
+                </Helmet>
 
-            <MenuContainer>
-                <Link to='/'>home</Link>
-                <Link to='/blog'>blog</Link>
-                <Link to='/about-me'>bio</Link>
-            </MenuContainer>
-
-            <ContentContainer>
-                <MetaContainer />
-                { title && <h1>{title}</h1> }
-                { subTitle && <SubTitle>{subTitle}</SubTitle>}
-                { children }
-            </ContentContainer>
-        </Container>
+                    <MetaContainer />
+                    { title && <h1>{title}</h1> }
+                    { subTitle && <SubTitle>{subTitle}</SubTitle>}
+                    { children }
+            </StyledContainer>
+        </React.Fragment>
     )
 }
 
@@ -90,8 +66,9 @@ Page.propTypes = {
         PropTypes.array,
         PropTypes.object
     ]).isRequired,
-    title: PropTypes.string.isRequired,
-    subTitle: PropTypes.string
+    title: PropTypes.string,
+    subTitle: PropTypes.string,
+    text: PropTypes.bool,
 }
 
 export default Page
--- src/components/Layout/index.spec.js
+++ src/components/Layout/index.spec.js
@@ -7,9 +7,16 @@ const TEST_TITLE = 'Test'
 const TEST_SUBTITLE = 'test'
 
 describe('Layout', () => {
-    it('renders correctly', () => {
+    it('renders correctly with title', () => {
         const tree = renderer
             .create(<Layout title={TEST_TITLE} subTitle={TEST_SUBTITLE}><p>test</p></Layout>)
+            .toJSON()
+        expect(tree).toMatchSnapshot()
+    })
+
+    it('renders correctly without title', () => {
+        const tree = renderer
+            .create(<Layout><p>test</p></Layout>)
             .toJSON()
         expect(tree).toMatchSnapshot()
     })
