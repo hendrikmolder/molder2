7857a6ed7c8a0342e3ee57f5ff15c5aad2ca88d8
Hendrik Mölder <hendrik@molder.eu>
2019-06-07 12:35:52 +0300

    Configure robots

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,13 @@
 require('dotenv').config()
 const linkResolver = require('./src/utils/linkResolver')
+const {
+    NODE_ENV,
+    URL: NETLIFY_SITE_URL = 'https://www.example.com',
+    DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+    CONTEXT: NETLIFY_ENV = NODE_ENV
+  } = process.env;
+const isNetlifyProduction = NETLIFY_ENV === 'production';
+const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL;
 
 module.exports = {
     siteMetadata: {
@@ -39,6 +47,35 @@ module.exports = {
                 linkResolver: ({ node, key, value }) => doc => linkResolver(doc)
             }
         },
-        `gatsby-plugin-sitemap`
+        `gatsby-plugin-sitemap`,
+        {
+            resolve: 'gatsby-plugin-robots-txt',
+            options: {
+                resolveEnv: () => NETLIFY_ENV,
+                env: {
+                    production: {
+                        policy: [{
+                            userAgent: '*'
+                        }]
+                    },
+                    'branch-deploy': {
+                        policy: [{
+                            userAgent: '*',
+                            disallow: ['/']
+                        }],
+                        sitemap: null,
+                        host: null
+                    },
+                    'deploy-preview': {
+                        policy: [{
+                            userAgent: '*',
+                            disallow: ['/']
+                        }],
+                        sitemap: null,
+                        host: null
+                    }
+                }
+            }
+        }
     ]
 }
