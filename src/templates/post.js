8b5ced845c71acf289c6b74d54e0e1036ae4c7ac
Hendrik Mölder <hendrik@molder.eu>
2019-06-07 12:36:04 +0300

    Add meta fields for posts

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import Helmet from 'react-helmet'
+
 import Layout from '../components/Layout'
 
 export default ({ data: { prismicPosts } }) => {
@@ -10,6 +12,8 @@ export default ({ data: { prismicPosts } }) => {
         published_at: last_publication_date
     }
 
+    const previewText = data.body.text.substring(1, 200).concat("...")
+
 
     return (
         <React.Fragment>
@@ -19,6 +23,14 @@ export default ({ data: { prismicPosts } }) => {
                 meta={metaData}
                 text
             >
+                <Helmet>
+                    { data.preview_image.url && <meta property="og.:image" content={data.preview_image.url} /> }
+                    <meta property="description" content={previewText} />
+                    <meta property="og:title" content={data.title.text} />
+                    <meta property="og:type" content="article" />
+                    <meta property="article:author" content={metaData.name} />
+                    <meta property="article:published_time" content={metaData.published_at} />
+                </Helmet>
                 <div dangerouslySetInnerHTML={{ __html: data.body.html }} />
             </Layout>
         </React.Fragment>
@@ -33,7 +45,7 @@ export const pageQuery = graphql`
             data {
                 title { text }
                 subtitle { text }
-                body { html }
+                body { html text }
                 author {
                     document {
                         ...on PrismicPerson {
@@ -48,6 +60,7 @@ export const pageQuery = graphql`
                         }
                     }
                 }
+                preview_image { url }
             }
         }
     }
